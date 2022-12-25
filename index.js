f40b14372c5f4f7b6facd25138b0311ac2711639
ji-noppon <n.jirasirichot@gmail.com>
2022-12-26 00:48:42 +0700

    add: patch users

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require('express')
-
+const database = require('./models/userModel')
 const app = express()
 
 const PORT = 3500
 
 app.get('/',(req,res)=>{
-    res.json([
-        {
-            'name':'kiyoshi',
-            'lastname':'numpa'
-        },
-        {
-            'name':'Gymolia',
-            'lastname':'kasiya'
+    
+   res.send("welcome to cookie shop")
+   
+})
+
+app.get('/users',(req,res)=>{
+    var query = "select * from customers"
+    database.query(query,(error,data)=>{
+        if(error){
+            throw error;
+        }else{
+            /* console.log(data); */
+            res.json(data)
         }
-    ]
-           
-    )
+    })
+   
+})
+
+app.get('/users/:id',(req,res)=>{
+    var id = req.params.id
+    console.log(id);
+    var query = `select * from customers    \
+                 where customer_id = ${id} `
+    database.query(query,(error,data)=>{
+        if(error){
+            throw error;
+        }else{
+            /* console.log(data); */
+            res.json(data)
+        }
+    })
+   
 })
 
 app.listen(PORT,()=>{
