0893b2a72be499889b188869b4d29dc31a59af2e
ji-noppon <n.jirasirichot@gmail.com>
2023-01-03 00:18:27 +0700

    add: Routing

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const express = require('express')
-const database = require('./models/userModel')
 const app = express()
 
+const {router} = require('./routes/routing')    //module.export = {router}
 const PORT = 3500
-const {users} = require('./controller/users/users')
-const {usersId} = require('./controller/users/usersId')
 
-app.get('/',(req,res)=>{
-   res.send("welcome to cookie shop") 
-})
-
-app.get('/users',users)
-
-app.get('/users/:id',usersId)
-
-app.all('*',(req,res)=>{
-    res.status(404).send('Not Found')
-})
+app.use(router)
 
 app.listen(PORT,()=>{
     console.log('====== node start =======');
---
+++ routes/routing.js
@@ -0,0 +1,24 @@
+const {users} = require('../controller/users/users')
+const {usersId} = require('../controller/users/usersId')
+const {Router} = require('express')   // import
+const {logger,getStatusOnFinish} = require('../log/logger')
+
+const router = Router()     // Initialization
+
+router.use(logger)
+
+router.get('/',(req,res)=>{
+    res.send("welcome to cookie shop") 
+    getStatusOnFinish(res.statusCode)
+ })
+ 
+ router.get('/users',users)
+ 
+ router.get('/users/:id',usersId)
+ 
+ router.all('*',(req,res)=>{
+     res.status(404).send('Not Found')
+     getStatusOnFinish(res.statusCode)
+ })
+
+module.exports = {router}
